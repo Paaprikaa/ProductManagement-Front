80fe73dc5f5a5a95a51788af8cfee822e0470237
Paaprikaa <melina.morales@mi.unc.edu.ar>
2023-11-16 16:35:19 -0300

    feat: basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { PRODUCTS } from "./utils/constants";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <table>
+        <tr>
+          <th>CANTIDAD</th>
+          <th>DESCRIPCIÓN</th>
+          <th>SUBTOTAL</th>
+          <th>TOTAL</th>
+          <th></th>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Fernet</td>
+          <td>150</td>
+          <td>300</td>
+          <td>quitar</td>
+        </tr>
+      </table>
+      <div className="app-bottom-row">
+        <div className="select-new-product">
+          <h3>Seleccionar nuevo producto</h3>
+          <div className="product-list">
+            {Object.entries(PRODUCTS).map((product) => {
+              return (
+                <input
+                  type="button"
+                  value={`${product[1][1]}: $${product[1][0]}`}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <div className="purchase-details">
+          <h3>Detalles de compra</h3>
+          <div>Cantidad de items: </div>
+          <div>Precio total: </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const PRODUCTS = {
+  arrozBlanco: [100, "Arroz blanco"],
+  gaseosa: [80, "Gaseosa"],
+  fernet: [150, "Fernet"],
+  pan: [25, "Pan"],
+  cafe: [50, "Café"],
+};
